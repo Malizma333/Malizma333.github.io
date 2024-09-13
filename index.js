d45f7415cfe000d292fe1258afa6ac7f711e26f6
Malizma333 <malizma333@gmail.com>
2024-09-12 20:15:38 -0500

    copy socials to clipboard

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function showOverlay(panelId) {
-  const overlay = document.getElementById("overlay")
-  overlay.style["opacity"] = 1;
-  overlay.style["pointerEvents"] = "all";
+function copyText(text) {
+  var blob = new Blob([text], {type: 'text/plain'});
+  var item = new ClipboardItem({'text/plain': blob});
+  navigator.clipboard.write([item]);
 }
 
-function hideOverlay() {
-  const overlay = document.getElementById("overlay")
-  overlay.style["opacity"] = 0;
-  overlay.style["pointerEvents"] = "none";
+window.onload = function() {
+  const emailContactLink = document.getElementById("contacts-email");
+  const discordContactLink = document.getElementById("contacts-discord");
+  
+  emailContactLink.addEventListener("click", async (e) => {
+    e.preventDefault();
+    copyText("tkbessler@gmail.com");
+  }, false);
+
+  discordContactLink.addEventListener("click", async (e) => {
+    e.preventDefault();
+    copyText("malizma");
+  }, false);
 }
