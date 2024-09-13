f73c297bdd5098c2ff5cdef97161b93a15e2b8cd
Malizma333 <malizma333@gmail.com>
2024-09-12 20:33:54 -0500

    copy ui

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-function copyText(text) {
+async function copyEvent(event, contactRef, text) {
+  event.preventDefault();
+
+  const copyIcon = contactRef.getElementsByClassName("fa-copy")[0];
+  
   var blob = new Blob([text], {type: 'text/plain'});
   var item = new ClipboardItem({'text/plain': blob});
-  navigator.clipboard.write([item]);
+  await navigator.clipboard.write([item]);
+
+  copyIcon.classList.add("fa-check");
+  contactRef.style.pointerEvents = "none";
+  contactRef.style.cursor = "default";
+
+  setTimeout(() => {
+    copyIcon.classList.remove("fa-check");
+    contactRef.style.pointerEvents = "all";
+    contactRef.style.cursor = "pointer";
+  }, 500);
 }
 
 window.onload = function() {
   const emailContactLink = document.getElementById("contacts-email");
   const discordContactLink = document.getElementById("contacts-discord");
   
-  emailContactLink.addEventListener("click", async (e) => {
-    e.preventDefault();
-    copyText("tkbessler@gmail.com");
-  }, false);
+  emailContactLink.addEventListener(
+    "click",
+    async (e) => copyEvent(e, emailContactLink, "tkbessler@gmail.com"),
+    false
+  );
 
-  discordContactLink.addEventListener("click", async (e) => {
-    e.preventDefault();
-    copyText("malizma");
-  }, false);
+  discordContactLink.addEventListener(
+    "click",
+    async (e) => copyEvent(e, discordContactLink, "malizma"),
+    false
+  );
 }
