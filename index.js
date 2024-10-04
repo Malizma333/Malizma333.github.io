7991b12752922b4f9c899a883d0f26bf006befcc
Malizma333 <malizma333@gmail.com>
2024-10-03 23:50:51 -0500

    header name svg

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-const module = (function() {
-  let currentCard = undefined;
+(function () {
+  let currentCard
 
   const projectsArray = [
     {
-      titleText: "Line Rider Web",
-      description: "[Line Rider Web](https://www.linerider.com) is a revision of the 2006 flash game Line Rider built for the web. I contributed highly-requested features and bugfixes.",
-      thumbSrc: "assets/lrweb-thumb-cropped.png",
-      thumbAlt: "Thumbnail for the official Line Rider website.",
-      devIcons: [["devicon-react-original", "React"], ["devicon-materialui-plain", "MaterialUI"], ["devicon-bash-plain","Bash"]]
+      titleText: 'Line Rider Web',
+      description: '[Line Rider Web](https://www.linerider.com) is a revision of the 2006 flash game Line Rider built for the web. I contributed highly-requested features and bugfixes.',
+      thumbSrc: 'assets/lrweb-thumb-cropped.png',
+      thumbAlt: 'Thumbnail for the official Line Rider website.',
+      devIcons: [['devicon-react-original', 'React'], ['devicon-materialui-plain', 'MaterialUI'], ['devicon-bash-plain', 'Bash']]
     },
     {
-      titleText: "Line Rider Desktop",
+      titleText: 'Line Rider Desktop',
       description: "Line Rider Desktop is another revision of Line Rider built as a Windows desktop app. I've assisted with cross-compatibility issues and feature requests. The download is publicly available at [this GitHub repository](https://github.com/LunaKampling/LROverhaul).",
-      thumbSrc: "assets/lra-thumb-cropped.png",
-      thumbAlt: "Banner for the desktop edition of Line Rider.",
-      devIcons: [["devicon-csharp-plain", "C#"]]
+      thumbSrc: 'assets/lra-thumb-cropped.png',
+      thumbAlt: 'Banner for the desktop edition of Line Rider.',
+      devIcons: [['devicon-csharp-plain', 'C#']]
     },
     {
-      titleText: "Line Rider Web Guide",
-      description: "The [Line Rider web guide](https://malizma333.github.io/line-rider-web-guide) is a collection of articles and tutorials oriented towards heavier users of Line Rider web.",
-      thumbSrc: "assets/lrdocs-thumb-cropped.png",
-      thumbAlt: "Screenshot of the Line Rider web guide.",
-      devIcons: [["devicon-markdown-original", "Markdown"], ["devicon-jekyll-plain", "Jekyll"]]
+      titleText: 'Line Rider Web Guide',
+      description: 'The [Line Rider web guide](https://malizma333.github.io/line-rider-web-guide) is a collection of articles and tutorials oriented towards heavier users of Line Rider web.',
+      thumbSrc: 'assets/lrdocs-thumb-cropped.png',
+      thumbAlt: 'Screenshot of the Line Rider web guide.',
+      devIcons: [['devicon-markdown-original', 'Markdown'], ['devicon-jekyll-plain', 'Jekyll']]
     },
     {
-      titleText: "Userscript Mods",
+      titleText: 'Userscript Mods',
       description: "Line Rider Web offers mod support through userscripts that inject JavaScript into the site. Userscript-based mods I've developed can be found at [this repository](https://github.com/Malizma333/linerider-userscript-mods).",
-      thumbSrc: "assets/userscript-thumb-cropped.png",
-      thumbAlt: "Screenshot of the Tampermonkey dashboard, showcasing a list of Line Rider related userscripts.",
-      devIcons: [["devicon-javascript-plain", "JavaScript"]]
+      thumbSrc: 'assets/userscript-thumb-cropped.png',
+      thumbAlt: 'Screenshot of the Tampermonkey dashboard, showcasing a list of Line Rider related userscripts.',
+      devIcons: [['devicon-javascript-plain', 'JavaScript']]
     },
     {
-      titleText: "Kakuro Solver Webapp",
+      titleText: 'Kakuro Solver Webapp',
       description: "Kakuro is a pen-and-paper game that's best described as a hybrid of crosswords and sudoku. I made a simple puzzle solver to practice recursive backtracking and try out the NextJS framework. [Here's the website](https://kakuro-solver.vercel.app) hosted by Vercel.",
-      thumbSrc: "assets/kakuro-thumb-cropped.png",
-      thumbAlt: "Partial screenshot of a solved kakuro board.",
-      devIcons: [["devicon-nextjs-plain", "NextJS"], ["devicon-tailwindcss-original", "TailwindCSS"]]
+      thumbSrc: 'assets/kakuro-thumb-cropped.png',
+      thumbAlt: 'Partial screenshot of a solved kakuro board.',
+      devIcons: [['devicon-nextjs-plain', 'NextJS'], ['devicon-tailwindcss-original', 'TailwindCSS']]
     },
     {
-      titleText: "Quantum Tic-Tac-Toe",
-      description: "Quantum Tic-Tac-Toe is like classical Tic-Tac-Toe, but you can play two move states that get entangled. This was made as a final project for the Qubit x Qubit quantum computing course. A [web-ported version of the game](https://malizma.itch.io/quantum-tic-tac-toe) is hosted on itch.io.",
-      thumbSrc: "assets/quantum-thumb-cropped.png",
-      thumbAlt: "Screenshot of a quantum tic-tac-toe board.",
-      devIcons: [["devicon-unity-plain", "Unity"]]
+      titleText: 'Quantum Tic-Tac-Toe',
+      description: 'Quantum Tic-Tac-Toe is like classical Tic-Tac-Toe, but you can play two move states that get entangled. This was made as a final project for the Qubit x Qubit quantum computing course. A [web-ported version of the game](https://malizma.itch.io/quantum-tic-tac-toe) is hosted on itch.io.',
+      thumbSrc: 'assets/quantum-thumb-cropped.png',
+      thumbAlt: 'Screenshot of a quantum tic-tac-toe board.',
+      devIcons: [['devicon-unity-plain', 'Unity']]
     },
     {
-      titleText: "Rooftop Bebop",
+      titleText: 'Rooftop Bebop',
       description: "Rooftop Bebop is a 2D survival shooter game made in 2 weeks for the DigiPen summer game development course. You can [download the game](https://games.digipen.edu/games/rooftop-bebop) in DigiPen's gallery.",
-      thumbSrc: "assets/rooftopbebop-thumb-cropped.png",
-      thumbAlt: "Screenshot of Rooftop Bebop gameplay.",
-      devIcons: [["devicon-unity-plain", "Unity"]]
+      thumbSrc: 'assets/rooftopbebop-thumb-cropped.png',
+      thumbAlt: 'Screenshot of Rooftop Bebop gameplay.',
+      devIcons: [['devicon-unity-plain', 'Unity']]
     }
-  ];
+  ]
 
-  function generateProjectCard({
+  function generateProjectCard ({
     thumbAlt,
     thumbSrc,
     titleText,
     description,
     devIcons
   }) {
-    const icons = devIcons.map(icon => `<i class="${icon[0]}" title="${icon[1]}"></i>`).join(' ');
-    const newDescription = description.replace(/[\[](.+?)[\]][\(](.+?)[\)]/, "<a href=\"$2\">$1</a>");
+    const icons = devIcons.map(icon => `<i class="${icon[0]}" title="${icon[1]}"></i>`).join(' ')
+    const newDescription = description.replace(/[[](.+?)[\]][(](.+?)[)]/, '<a href="$2">$1</a>')
     return `
     <div class="project-container">
       <img alt="${thumbAlt}" src="${thumbSrc}"/>
@@ -78,74 +78,82 @@ const module = (function() {
           <p class="p-text description">${newDescription}</p>
         </div>
       </div>
-    </div>`;
+    </div>`
   }
 
-  async function copyContact(event, contactRef, text) {
-    event.preventDefault();
-    const copyIcon = contactRef.getElementsByClassName("fa-copy")[0];
+  async function copyContact (event, contactRef, text) {
+    event.preventDefault()
+    const copyIcon = contactRef.getElementsByClassName('fa-copy')[0]
 
-    var blob = new Blob([text], { type: "text/plain" });
-    var item = new ClipboardItem({ "text/plain": blob });
+    const blob = new Blob([text], { type: 'text/plain' })
+    const item = new window.ClipboardItem({ 'text/plain': blob })
 
-    await navigator.clipboard.write([item]);
+    await navigator.clipboard.write([item])
 
-    copyIcon.classList.add("fa-check");
-    contactRef.style.pointerEvents = "none";
-    contactRef.style.cursor = "default";
+    copyIcon.classList.add('fa-check')
+    contactRef.style.pointerEvents = 'none'
+    contactRef.style.cursor = 'default'
 
-    await new Promise(_ => setTimeout(_, 500));
+    await new Promise(resolve => setTimeout(resolve, 500))
 
-    copyIcon.classList.remove("fa-check");
-    contactRef.style.pointerEvents = "all";
-    contactRef.style.cursor = "pointer";
+    copyIcon.classList.remove('fa-check')
+    contactRef.style.pointerEvents = 'all'
+    contactRef.style.cursor = 'pointer'
   }
 
-  function showCard(event, nextCard) {
-    event.preventDefault();
+  function showCard (event, nextCard) {
+    event.preventDefault()
 
-    if(currentCard !== undefined) {
-      currentCard.style.margin = "-100% 0%";
-      const textContent = currentCard.querySelector(".project-text");
-      console.log(textContent);
-      if(textContent) textContent.scrollTo(0,0);
+    if (currentCard !== undefined) {
+      currentCard.style.margin = '-100% 0%'
+      const textContent = currentCard.querySelector('.project-text')
+      console.log(textContent)
+      if (textContent) textContent.scrollTo(0, 0)
     }
 
-    currentCard = nextCard;
+    currentCard = nextCard
 
-    if(nextCard !== undefined) {
-      currentCard.style.margin = "0% 0%";
+    if (nextCard !== undefined) {
+      currentCard.style.margin = '0% 0%'
     }
   }
 
-  window.onload = function() {
-    const emailContactLink = document.getElementById("contacts-email");
-    const discordContactLink = document.getElementById("contacts-discord");
-    
+  window.onload = function () {
+    const emailContactLink = document.getElementById('contacts-email')
+    const discordContactLink = document.getElementById('contacts-discord')
+
     emailContactLink.addEventListener(
-      "click", async (e) => copyContact(e, emailContactLink, "tkbessler@gmail.com"), false
-    );
+      'click', async (e) => copyContact(e, emailContactLink, 'tkbessler@gmail.com'), false
+    )
 
     discordContactLink.addEventListener(
-      "click", async (e) => copyContact(e, discordContactLink, "malizma"), false
-    );
+      'click', async (e) => copyContact(e, discordContactLink, 'malizma'), false
+    )
 
-    const projectDiv = document.getElementById("projects");
+    const projectDiv = document.getElementById('projects')
 
-    for(const projectData of projectsArray) {
-      projectDiv.innerHTML += generateProjectCard(projectData);
+    for (const projectData of projectsArray) {
+      projectDiv.innerHTML += generateProjectCard(projectData)
     }
 
-    const projectCells = document.getElementsByClassName("project-container");
+    const projectCells = document.getElementsByClassName('project-container')
 
-    for(const project of projectCells) {
-      const card = project.getElementsByClassName("project-content")[0];
-      const openButton = project.getElementsByClassName("project-thumb")[0];
-      const closeButton = project.getElementsByClassName("project-content-close")[0];
-      openButton.addEventListener("click", (e) => showCard(e, card), false);
-      closeButton.addEventListener("click", (e) => showCard(e, undefined), false);
+    for (const project of projectCells) {
+      const card = project.getElementsByClassName('project-content')[0]
+      const openButton = project.getElementsByClassName('project-thumb')[0]
+      const closeButton = project.getElementsByClassName('project-content-close')[0]
+      openButton.addEventListener('click', (e) => showCard(e, card), false)
+      closeButton.addEventListener('click', (e) => showCard(e, undefined), false)
     }
+
+    const headerCurtain = document.getElementById('name-header-curtain')
+    const headerName = document.getElementById('name-header-svg')
+    const contentElement = document.querySelector('div[class="content"]')
+
+    contentElement.addEventListener('scroll', (event) => {
+      headerCurtain.style.top = `${Math.min(event.target.scrollTop, headerName.clientHeight)}px`
+    })
   }
 
-  return undefined;
-})();
+  return undefined
+})()
