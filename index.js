fbce98ba1dd51cfe00b50c9f6203711b7e229e7d
Malizma333 <malizma333@gmail.com>
2024-10-26 17:10:21 -0500

    temp commit again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function () {
-  let currentCard
+  let currentCard, lastHash
 
   const projectsArray = [
     {
@@ -117,64 +117,6 @@
     }
   }
 
-  function initCanvas () {
-    const canvas = document.getElementById('main-canvas')
-    const ctx = canvas.getContext('2d')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-
-    window.addEventListener('resize', () => {
-      canvas.width = window.innerWidth
-      canvas.height = window.innerHeight
-    }, false)
-
-    window.addEventListener('mousemove', (e) => {
-      const rect = canvas.getBoundingClientRect()
-      const mouse = {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-      }
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.save()
-
-      // Draw mouse clip
-      ctx.beginPath()
-      ctx.arc(mouse.x, mouse.y, 50, 0, 2 * Math.PI)
-      ctx.closePath()
-      ctx.clip()
-
-      // Draw background
-      ctx.strokeStyle = "white"
-      ctx.lineWidth = 2
-      const lineCount = 20
-      const delta = canvas.height / lineCount
-      ctx.beginPath()
-      for(let i = 0; i <= canvas.height; i += delta) {
-        ctx.moveTo(0, i)
-        ctx.lineTo(canvas.width, i)
-      }
-      for(let i = 0; i <= canvas.width; i += delta) {
-        ctx.moveTo(i, 0)
-        ctx.lineTo(i, canvas.height)
-      }
-      ctx.closePath()
-      ctx.stroke()
-
-      // Apply clip
-      ctx.restore()
-    }, false)
-
-    canvas.addEventListener('click', (e) => {
-      e.preventDefault()
-      const a = document.createElement('a')
-      a.href = 'https://minesweeper.online/'
-      a.click()
-      a.remove()
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-    }, false)
-  }
-
   function setupLinks () {
     const emailContactLink = document.getElementById('contacts-email')
     const discordContactLink = document.getElementById('contacts-discord')
@@ -214,10 +156,28 @@
     })
   }
 
+  function initStep () {
+    document.querySelector('main').style.display = 'none'
+    CanvasHelper.load()
+  }
+
   window.onload = function () {
-    initCanvas()
+    CanvasHelper.initCanvas()
     setupLinks()
     createProjects()
+
+    lastHash = document.location.hash
+    if (lastHash === '#minesweeper') {
+      initStep()
+    }
+
+    window.navigation.addEventListener("navigate", (e) => {
+      console.log(lastHash, document.location.hash)
+      if (lastHash !== document.location.hash && document.location.hash === '#minesweeper') {
+        lastHash = document.location.hash
+        initStep()
+      }
+    })
   }
 
   return undefined
