38c4d2f7fa4ebe9068f97a29de314117d2e39f7a
tkbessler <tkbessler@gmail.com>
2024-10-27 15:53:37 -0500

    back transition

--- canvas.js
+++ canvas.js
@@ -2,17 +2,24 @@ class CanvasHelper {
   static MODES = {
     PREVIEW: 0,
     EXPAND: 1,
-    GAME: 2
+    GAME: 2,
+    CONTRACT: 3
   }
+
+  static gridSize = 100
+  static pixelSize = 30
+  static transitionDelta = 50
 
   static initCanvas () {
     this.canvas = document.getElementById('main-canvas')
+    this.main = document.getElementById('main-content')
     this.ctx = this.canvas.getContext('2d')
     this.canvas.width = window.innerWidth
     this.canvas.height = window.innerHeight
     this.mode = this.MODES.PREVIEW
     this.pathRadius = 50
-    this.pathRadiusDelta = 50
+    this.grid = Array(this.gridSize).fill().map(() => Array(this.gridSize).fill(0))
+    this.previewSeenFlag = false
 
     window.addEventListener('resize', () => {
       this.canvas.width = window.innerWidth
@@ -20,69 +27,124 @@ class CanvasHelper {
       this.redraw()
     }, false)
 
-    window.addEventListener('mousemove', () => {
+    window.addEventListener('mousemove', (e) => {
       if (this.mode === this.MODES.PREVIEW) {
-        this.redraw()
+        this.redraw(e)
       }
     }, false)
 
-    this.canvas.addEventListener('click', () => {
-      if (this.mode !== this.MODES.PREVIEW) return
-      document.querySelector('main').style['pointer-events'] = 'none'
-      this.mode = this.MODES.EXPAND
-      this.redraw()
+    this.canvas.addEventListener('click', (e) => {
+      if (this.mode === this.MODES.PREVIEW) {
+        document.body.style['pointer-events'] = 'none'
+        this.mode = this.MODES.EXPAND
+        this.redraw(e)
+      }
+
+      if (this.mode === this.MODES.GAME) {
+        this.redraw(e)
+      }
+    }, false)
+
+    document.getElementById('back-button').addEventListener('click', (e) => {
+      if (this.mode === this.MODES.GAME) {
+        document.body.style['pointer-events'] = 'none'
+        this.mode = this.MODES.CONTRACT
+        this.redraw(e)
+      }
     }, false)
   }
 
-  static redraw () {
+  static redraw (e) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    if (this.mode === this.MODES.PREVIEW) {
-      const rect = this.canvas.getBoundingClientRect()
-      const mouse = {
-        x: window.event.clientX - rect.left,
-        y: window.event.clientY - rect.top
+    const rect = this.canvas.getBoundingClientRect()
+    let mouse
+    if (e) {
+      mouse = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
       }
-      this.drawClipPath(mouse, this.canvas, this.ctx)
-    } else if (this.mode === this.MODES.EXPAND) {
-      const rect = this.canvas.getBoundingClientRect()
-      const mouse = {
-        x: window.event.clientX - rect.left,
-        y: window.event.clientY - rect.top
+    }
+
+    switch (this.mode) {
+      case this.MODES.PREVIEW: {
+        if (mouse) {
+          this.drawClipPath(mouse, this.canvas, this.ctx)
+        }
+        if (this.previewSeenFlag) {
+          this.drawBoard(this.canvas, this.ctx)
+        }
+        break
       }
-      this.transition(mouse, this.canvas, this.ctx)
-    } else {
-      this.drawBoard(this.canvas, this.ctx)
+
+      case this.MODES.EXPAND: {
+        if (mouse) {
+          this.assignClick(mouse)
+          this.transition(mouse)
+        }
+        break
+      }
+
+      case this.MODES.GAME: {
+        if (mouse) {
+          this.assignClick(mouse)
+        }
+        this.drawBoard(this.canvas, this.ctx)
+        break
+      }
+
+      case this.MODES.CONTRACT: {
+        this.drawBoard(this.canvas, this.ctx)
+        this.reverseTransition(this.canvas, this.ctx)
+      }
     }
   }
 
   static drawBoard (canvas, ctx) {
     ctx.strokeStyle = 'white'
     ctx.lineWidth = 1
-    const sideLen = 30
     const origin = [canvas.width / 2, canvas.height / 2]
     const dimensions = [
-      Math.floor(canvas.width / (2 * sideLen)) + 2,
-      Math.floor(canvas.height / (2 * sideLen)) + 2
+      Math.floor(canvas.width / (2 * this.pixelSize)) + 2,
+      Math.floor(canvas.height / (2 * this.pixelSize)) + 2
     ]
 
     ctx.beginPath()
 
     for (let i = 0; i <= dimensions[1]; i++) {
-      ctx.moveTo(origin[0] - dimensions[0] * sideLen, origin[1] + i * sideLen)
-      ctx.lineTo(origin[0] + dimensions[0] * sideLen, origin[1] + i * sideLen)
-      ctx.moveTo(origin[0] - dimensions[0] * sideLen, origin[1] - i * sideLen)
-      ctx.lineTo(origin[0] + dimensions[0] * sideLen, origin[1] - i * sideLen)
+      ctx.moveTo(origin[0] - dimensions[0] * this.pixelSize, origin[1] + i * this.pixelSize)
+      ctx.lineTo(origin[0] + dimensions[0] * this.pixelSize, origin[1] + i * this.pixelSize)
+      ctx.moveTo(origin[0] - dimensions[0] * this.pixelSize, origin[1] - i * this.pixelSize)
+      ctx.lineTo(origin[0] + dimensions[0] * this.pixelSize, origin[1] - i * this.pixelSize)
     }
 
     for (let i = 0; i <= dimensions[0]; i++) {
-      ctx.moveTo(origin[0] + i * sideLen, origin[1] - dimensions[1] * sideLen)
-      ctx.lineTo(origin[0] + i * sideLen, origin[1] + dimensions[1] * sideLen)
-      ctx.moveTo(origin[0] - i * sideLen, origin[1] - dimensions[1] * sideLen)
-      ctx.lineTo(origin[0] - i * sideLen, origin[1] + dimensions[1] * sideLen)
+      ctx.moveTo(origin[0] + i * this.pixelSize, origin[1] - dimensions[1] * this.pixelSize)
+      ctx.lineTo(origin[0] + i * this.pixelSize, origin[1] + dimensions[1] * this.pixelSize)
+      ctx.moveTo(origin[0] - i * this.pixelSize, origin[1] - dimensions[1] * this.pixelSize)
+      ctx.lineTo(origin[0] - i * this.pixelSize, origin[1] + dimensions[1] * this.pixelSize)
     }
 
     ctx.closePath()
     ctx.stroke()
+
+    if (this.mode === this.MODES.PREVIEW && !this.previewSeenFlag) return
+
+    ctx.fillStyle = 'white'
+    ctx.beginPath()
+
+    for (let i = 0; i < this.gridSize; i++) {
+      for (let j = 0; j < this.gridSize; j++) {
+        if (this.grid[i][j] === 0) continue
+        const target = [
+          (i - this.gridSize / 2) * this.pixelSize + this.canvas.width / 2,
+          (j - this.gridSize / 2) * this.pixelSize + this.canvas.height / 2
+        ]
+        ctx.rect(target[0], target[1], this.pixelSize, this.pixelSize)
+      }
+    }
+
+    ctx.closePath()
+    ctx.fill()
   }
 
   static drawClipPath (mouse, canvas, ctx) {
@@ -96,17 +158,54 @@ class CanvasHelper {
   }
 
   static transition (mouse) {
-    this.pathRadius += this.pathRadiusDelta
-    this.drawClipPath(mouse, this.canvas, this.ctx)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.pathRadius += this.transitionDelta
+
+    if (!this.previewSeenFlag) {
+      this.drawClipPath(mouse, this.canvas, this.ctx)
+    } else {
+      this.drawBoard(this.canvas, this.ctx)
+    }
 
     const target = Math.pow(this.canvas.width, 2) + Math.pow(this.canvas.height, 2)
     const progress = Math.pow(this.pathRadius, 2) / target
-    document.querySelector('main').style.marginTop = `-${progress * 110}vh`
+    this.main.style.marginTop = `-${progress * 110}vh`
 
     if (progress < 1) {
       window.requestAnimationFrame(() => this.transition(mouse))
     } else {
       this.mode = this.MODES.GAME
+      this.previewSeenFlag = true
+      document.body.style['pointer-events'] = 'auto'
     }
+  }
+
+  static reverseTransition () {
+    this.pathRadius -= this.transitionDelta
+
+    const target = Math.pow(this.canvas.width, 2) + Math.pow(this.canvas.height, 2)
+    const progress = Math.pow(this.pathRadius, 2) / target
+    this.main.style.marginTop = `-${progress * 110}vh`
+
+    if (progress > 0) {
+      window.requestAnimationFrame(() => this.reverseTransition())
+    } else {
+      this.main.style.marginTop = '0'
+      this.mode = this.MODES.PREVIEW
+      document.body.style['pointer-events'] = 'auto'
+    }
+  }
+
+  static assignClick (mouse) {
+    const index = [
+      Math.floor((mouse.x - this.canvas.width / 2) / this.pixelSize) + this.gridSize / 2,
+      Math.floor((mouse.y - this.canvas.height / 2) / this.pixelSize) + this.gridSize / 2
+    ]
+
+    if (index[0] < 0 || index[0] >= this.gridSize || index[1] < 0 || index[1] >= this.gridSize) {
+      return
+    }
+
+    this.grid[index[0]][index[1]] = 1 - this.grid[index[0]][index[1]]
   }
 }
